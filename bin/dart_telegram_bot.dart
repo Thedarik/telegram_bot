fdb2807c87ab43107dae6a97bf0e021690be975c
thedarik <thedarik2021@gmail.com>
2025-05-18 23:33:12 +0500

    api tokon

--- bin/dart_telegram_bot.dart
+++ bin/dart_telegram_bot.dart
@@ -24,7 +24,7 @@ import 'bot/statistika_menu.dart';
 import 'bot/aloqa_menu.dart';
 
 void main() async {
-  const String botToken = '7336719075:AAFScROTO5aJPsp6oud2gD9UJaR3KoSNvR8';
+  const String botToken = '7823203949:AAE5Ryfa6yAA3y0FKODW7nPKwUtKaW5QaJg';
   final telegram = Telegram(botToken);
   final username = (await telegram.getMe()).username;
   final teledart = TeleDart(botToken, Event(username!));
